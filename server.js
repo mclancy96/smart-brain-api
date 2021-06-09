0adf46de82aa227b1f8b2485f01e32e43ca1a611
Mike <mclancy96@gmail.com>
2021-06-09 15:10:15 -0400

    Connect to frontend and server

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    user : 'postgres',
+    password : 'postgres',
+    database : 'smartbrain'
   }
 });
 
@@ -32,6 +32,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3004, ()=> {
+app.listen(3003, ()=> {
   console.log('app is running on port 3000');
 })
